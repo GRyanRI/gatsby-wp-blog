c01247c15d4650c550c939965853415f3b5c0186
GRyanRI <gregorywryan@gmail.com>
2019-07-16 21:14:37 -0400

    Finished initial layout using react-bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import PrimaryLayout from "../layouts/PrimaryLayout"
+import Post from '../components/Post'
+
+export default () => (
+  <PrimaryLayout column="col-xs-6">
+    <Post 
+      title="This is my first post" 
+      excerpt="Writing that will be displayed in excerpt"
+    />
+    <Post 
+      title="This is my second post" 
+      excerpt="Writing that will be displayed in excerpt"
+    />
+    <Post 
+      title="This is my third post" 
+      excerpt="Writing that will be displayed in excerpt"
+    />
+  </PrimaryLayout>
+)
